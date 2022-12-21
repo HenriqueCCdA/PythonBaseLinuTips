cd619ba553e3780efdf88c10bf3b0c1b613378ad
Henrique Andrade <henrique.ccda@gmail.com>
2022-12-21 12:36:37 -0300

    Infixcalc.

--- hello.py
+++ hello.py
@@ -11,26 +11,51 @@ Tenha a variável LANG devidamente configurada ex:
 
 	export LANG=pt_BR
 
+Ou informe atraves do CLI argument `--lang`
+
+
 Execução:
 
 	python hello.py
 	ou
 	./hello.py
 """
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Henrique de Andrade'
 __license__ = 'Unlicense'
 
 import os
+import sys
 
-current_language = os.getenv('LANG', 'en_US')[:5]
+arguments = {'lang': None, 'count': 1}
+
+for arg in sys.argv[1:]:
+	# TODO: Tratar ValueError
+	key, value = arg.split('=')
+	key = key.lstrip('-').strip()
+	value = value.strip()
+	if key not in arguments:
+		print(f'Invalid Option `{key}`')
+		sys.exit()
+	print(key, value)
+	arguments[key] = value
+
+current_language = arguments['lang']
+if current_language is None:
+	# TODO: User repetição
+	if "LANG" in os.environ:
+		current_language = os.getenv('LANG') 
+	else:
+		current_language = input('Chosse a language:')
+	
+current_language = current_language[:5]
 
 msg = {
     'en_US': 'Hello, World!',
     'pt_BR': 'Olá, Mundo!',
     'it_IT': 'Ciao, Mondo!',
-    'es_SP': 'Hola, Mundo!',
+    'es_ES': 'Hola, Mundo!',
     'fr_FR': 'Bonjour, Monde!',
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments['count']))
