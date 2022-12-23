195aeaef7f6d57a30f80afa16cc4dfc675610840
Henrique Andrade <henrique.ccda@gmail.com>
2022-12-23 14:52:45 -0300

    Error: EAFP

--- erros.py
+++ erros.py
@@ -4,15 +4,33 @@ import sys
 
 # LBYL - Look Before You Leap
 
-if os.path.exists('names1.txt'):
-	names = open('names1.txt').readlines()
+# if os.path.exists('names1.txt'):
+	# names = open('names1.txt').readlines()
+# else:
+	# print('[Error] File names.txt not exists')
+	# sys.exit(1)
+
+# if len(names) >= 3:
+    # print(names[2])
+# else:
+    # print('[Error]: Missing name in the list')
+    # sys.exit(1)
+
+# EAFP - Easy to Ask Forgiveness than permission
+
+try:
+	names = open('names.txt').readlines()
+except FileNotFoundError as e:
+	print(f'[Error] {str(e)}.')
+	sys.exit(e.errno)
+	# TODO: User retry
 else:
-	print('[Error] File names.txt not exists')
+	print('Sucesso!!!')
+finally:
+	print('Execute isso sempre!')
+	
+try:
+    print(names[0])
+except:
+	print('[Error]: Missing name in the list')
 	sys.exit(1)
-
-
-if len(names) >= 3:
-    print(names[2])
-else:
-    print('[Error]: Missing name in the list')
-    sys.exit(1)
