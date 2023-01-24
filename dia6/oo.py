b4b25849d152965594a68a7766309e3d9de87c15
Henrique Andrade <henrique.ccda@gmail.com>
2023-01-24 18:31:28 -0300

    Dia 6.1

---
+++ dia6/oo.py
@@ -0,0 +1,23 @@
+people = [
+    {
+        "name": "Jim Halpert",
+        "balance": 500,
+        "role": "Salesman"
+    },
+    {
+        "name": "Dwight Schrute",
+        "balance": 100,
+        "role": "Manager"
+    }
+]
+
+def add_points(person, value):
+    data = person.copy()
+    if data["role"] == "Maneger":
+        value *= 2
+    data["balance"] += value
+    return data
+
+result = map(lambda person: add_points(person, 100), people)
+
+print(list(result))
