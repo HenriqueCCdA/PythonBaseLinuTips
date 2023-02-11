978100d369478160c9b16e1623e05f0fe81db75f
Henrique Andrade <henrique.ccda@gmail.com>
2023-02-11 15:09:57 -0300

    GUI

---
+++ dia8/tk_example.py
@@ -0,0 +1,28 @@
+from tkinter import StringVar, Tk, Label, Entry, Button
+
+janela = Tk()
+
+# widgets (components)
+label = Label(janela, text="Nome:")
+nome = Entry(janela)
+
+var = StringVar()
+mensagem = Label(janela, textvariable=var)
+
+
+def callback_diga_ola():
+    nome_digitado = nome.get()
+    var.set(f"Olá {nome_digitado} boas vindas!!")
+
+diga_ola = Button(janela, text="Diga Olá", command=callback_diga_ola)
+sair = Button(janela, text="Sair", command=janela.destroy)
+
+# Posiciona
+label.grid(column=1, row=1)
+nome.grid(column=2, row=1)
+mensagem.grid(column=2, row=2)
+diga_ola.grid(column=2, row=3)
+sair.grid(column=2, row=4)
+
+
+janela.mainloop()
